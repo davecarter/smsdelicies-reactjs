e8e7ff99166f0dc429c0ed0309e59cd73f00ec27
David G <david.garcia@scmspain.com>
2017-10-16 11:26:27 +0200

    adds literals file and rearrange files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import Topbar from './components/topbar'
+import Topbar from './components/topbar/index'
+import { es } from './components/literals'
 import './app.scss';
 
 class App extends Component {
   render() {
+    const { HOME_TITLE } = es
     return (
       <div className="App">
         <Topbar title='SMSDelicies' />
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>{HOME_TITLE}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/components/literals.js
@@ -0,0 +1,4 @@
+export const es = {
+  HOME_TITLE: 'Bienvenido a SMS Delicies',
+  HOME_SUBTITLE: 'Tu rincón gastronomico en Gràcia'
+}
