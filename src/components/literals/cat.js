4fbda9495910a6e707b039dd58b1e917c09f7a00
David G <david.garcia@scmspain.com>
2017-10-20 14:04:01 +0200

    adds ES literals

--- src/components/literals/cat.js
+++ src/components/literals/cat.js
@@ -1,5 +1,5 @@
 export const cat = {
-  "SITE_TITLE": "SMSDelicies",
-  "HOME_TITLE": "Benvingut a SMS Delicies",
-  "HOME_SUBTITLE": "El teu racó gastronómic a Gràcia"
+  'SITE_TITLE': 'SMSDelicies',
+  'HOME_TITLE': 'Benvingut a SMS Delicies',
+  'HOME_SUBTITLE': 'El teu racó gastronómic a Gràcia'
 }
--- src/components/literals/es.js
+++ src/components/literals/es.js
@@ -1,5 +1,16 @@
 export const es = {
-  "SITE_TITLE": "SMSDelicies",
-  "HOME_TITLE": "Bienvenido a SMS Delicies",
-  "HOME_SUBTITLE": "Tu rincón gastronomico en Gràcia"
+  'SITE_TITLE': 'SMSDelicies',
+  'HOME_TITLE': 'Bienvenido a SMS Delicies',
+  'HOME_SUBTITLE': 'Tu rincón gastronomico en Gràcia',
+  'HOME_DESCRIPTION_TITLE': 'Desde 2007',
+  'HOME_DESCRIPTION_CONTENT': 'SMS responde a las iniciales de <strong>Suculents Missatges amb Sabor</strong>, pequeñas porciones degustativas que contienen una delicia. En el año 2007 nació la idea de crear un espacio punto de encuentro donde tuviéramos la oportunidad de gozar de un producto de calidad y una atmósfera positiva durante todas las franjas horarias, desayunos, menús y comidas, meriendas y cenas. Un lugar al que a nosotros nos gustaría ir. Situado en la vía principal de acceso suroeste de la villa de Gracia, minimalista en la decoración y en su amplitud, se brinda a la luz de la calle a través de sus fachadas acristaladas. SMS Delícies abrió sus puertas buscando gente que compartiera nuestra misma idea.',
+  'HOME_BANNER_BREAKFAST_TITLE': 'Desayunos y meriendas',
+  'HOME_BANNER_BREAKFAST_TEXT': 'La constatación de un buen café se percibe desde el mismo aroma inicial. El estímulo de un té, aunque no sea el té de las cinco, te hará remontar hasta sus orígenes en la cultura del oriente más ancestral.',
+  'HOME_BANNER_BREAKFAST_BUTTON': 'Carta Desayunos',
+  'HOME_BANNER_MENU_TITLE': 'Menús y cenas',
+  'HOME_BANNER_MENU_TEXT': 'Mestiza e internacional, elaborada delante de tus ojos, nuestra íntima cocina combina y mezcla sabores que neutralizan el tiempo para aumentar el poder de la degustación. Déjate sorprender.',
+  'HOME_BANNER_MENU_BUTTON': 'Carta Menús y Cenas',
+  'HOME_BANNER_CATERING_TITLE': 'Catering',
+  'HOME_BANNER_CATERING_TEXT': 'Dentro de nuestra inquietud por diversificar, SMS Delícies creó al poco de abrir la línea de catering & eventos. Nuestros Food Event Designers se ocupan de crear un catering que se acople al evento de manera personalizada.',
+  'HOME_BANNER_CATERING_BUTTON': 'Carta Catering'
 }
